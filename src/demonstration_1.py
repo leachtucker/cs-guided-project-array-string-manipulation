c33b55fa8fa580fdf74075896c6ae286805df14c
Tucker <leachtucker@gmail.com>
2021-02-23 16:30:20 -0800

    Solved demo 1

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -22,4 +22,25 @@ There is no index that satisfies the conditions in the problem statement.
 """
 def pivot_index(nums):
     # Your code here
+    # Can easily be solved without a dictionary. That would require less space and might be a more efficient solution
+    # sum1 = left over our index i. sum2 = right of our index i
+    pivots = {} # key, code --> _int_ index, _tuple_ (sum1, sum2)
 
+    sum1 = 0
+    for i in range(0, len(nums)):
+        sum1 += nums[i]
+
+        sum2 = 0
+        for j in range(i, len(nums)):
+            sum2 += nums[j]
+
+        pivots[i] = (sum1, sum2)
+
+    for key, value in pivots.items():
+        if value[0] == value[1]:
+            return key
+
+    return -1
+
+print(pivot_index([1,7,3,6,5,6]))
+print(pivot_index([1,2,3]))
